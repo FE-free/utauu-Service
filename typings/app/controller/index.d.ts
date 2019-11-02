765019cb4b6783f252901bff1467942abfa4ee58
Allen OuYang <ouyangzhou@xylink.com>
2019-11-02 12:11:46 +0800

    style: change config

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -1,13 +1,13 @@
 // This file is created by egg-ts-helper@1.25.6
 // Do not modify this file!!!!!!!!!
 
-import "egg";
-import ExportArticle from "../../../app/controller/article";
-import ExportHome from "../../../app/controller/home";
-import ExportTags from "../../../app/controller/tags";
-import ExportUser from "../../../app/controller/user";
+import 'egg';
+import ExportArticle from '../../../app/controller/article';
+import ExportHome from '../../../app/controller/home';
+import ExportTags from '../../../app/controller/tags';
+import ExportUser from '../../../app/controller/user';
 
-declare module "egg" {
+declare module 'egg' {
   interface IController {
     article: ExportArticle;
     home: ExportHome;
